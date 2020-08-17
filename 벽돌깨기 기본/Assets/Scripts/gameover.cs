5afdf25217eda8f190c7fe6962633fe271e2fcc3
ArcRoot <phg5253@naver.com>
2020-08-17 22:15:21 +0900

    벽돌깨기 기본

---
+++ 벽돌깨기 기본/Assets/Scripts/gameover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameover : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("ball"))
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Spawn>().ball_count--;
+            if(GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Spawn>().ball_count==0)
+            {
+                GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Spawn>().destroy_evr();
+            }
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
